209bf1cf699ef7fc2a0d220b3e5c2ef21be94db0
jonty99 <jonty99@github.com>
2021-05-29 22:52:10 +0530

    new function add

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,8 @@
-print('Hello World')
+def addString(s1,s2):
+    print(type(s1))
+    return (str(int(s1)+int(s2)))
+print (addString("121","122"))
+print (addString("0","0"))
+
+
+
---
+++ add_string.py
@@ -0,0 +1,5 @@
+def addString(s1, s2):
+    print(type(s1))
+    return (str(int(s1) + int(s2)))
+print(addString("121", "122"))
+print(addString("0", "0"))
